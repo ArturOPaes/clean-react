8dc694839a769ea28250c9b45e9fb3fa7619443f
Artur Paes <arturopaes@gmail.com>
2020-09-02 22:13:11 -0300

    feat: ensure LocalStorageAdapter calls localStorage with correct values

---
+++ src/infra/cache/local-storage-adapter.ts
@@ -0,0 +1,8 @@
+import { SetStorage } from '@/data/protocols/cache/set-storage'
+
+export class LocalStorageAdapter implements SetStorage {
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async set(key: string, value: any): Promise<void> {
+    localStorage.setItem(key, value)
+  }
+}
