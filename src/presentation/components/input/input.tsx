a31dcb375bff2f9d19f2de72823cefb1b1243c24
Artur Paes <arturopaes@gmail.com>
2020-09-03 19:49:08 -0300

    feat: ensure SignUp starts with correct initial state

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -2,7 +2,10 @@ import React, { useContext } from 'react'
 import Styles from './input-styles.scss'
 import Context from '@/presentation/contexts/form/form-context'
 
-type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+type Props = React.DetailedHTMLProps<
+React.InputHTMLAttributes<HTMLInputElement>,
+HTMLInputElement
+>
 
 const Input: React.FC<Props> = (props: Props) => {
   const { state, setState } = useContext(Context)
@@ -17,15 +20,27 @@ const Input: React.FC<Props> = (props: Props) => {
     })
   }
   const getStatus = (): string => {
-    return error ? '🔴' : '🟢'
+    return error ? '🔴' : '🥳'
   }
   const getTitle = (): string => {
     return error || 'Tudo certo!'
   }
   return (
     <div className={Styles.inputWrap}>
-      <input {...props} data-testid={props.name} readOnly onFocus={enableInput} onChange={handleChange} />
-      <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
+      <input
+        {...props}
+        data-testid={props.name}
+        readOnly
+        onFocus={enableInput}
+        onChange={handleChange}
+      />
+      <span
+        data-testid={`${props.name}-status`}
+        title={getTitle()}
+        className={Styles.status}
+      >
+        {getStatus()}
+      </span>
     </div>
   )
 }
