5f4c5a4d44cae2d231217ac8a687882a93ba4cda
Artur Paes <arturopaes@gmail.com>
2020-09-04 00:25:34 -0300

    feat: ensure SingUp calls SaveAccessToken on success

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import Styles from './signup-styles.scss'
 import {
   Footer,
@@ -8,14 +9,20 @@ import {
 } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
 import { Validation } from '@/presentation/protocols/validation'
-import { AddAccount } from '@/domain/usecases'
+import { AddAccount, SaveAccessToken } from '@/domain/usecases'
 
 type Props = {
   validation: Validation
   addAccount: AddAccount
+  saveAccessToken: SaveAccessToken
 }
 
-const SignUp: React.FC<Props> = ({ validation, addAccount }: Props) => {
+const SignUp: React.FC<Props> = ({
+  validation,
+  addAccount,
+  saveAccessToken
+}: Props) => {
+  const history = useHistory()
   const [state, setState] = useState({
     isLoading: false,
     name: '',
@@ -60,12 +67,14 @@ const SignUp: React.FC<Props> = ({ validation, addAccount }: Props) => {
         return
       }
       setState({ ...state, isLoading: true })
-      await addAccount.add({
+      const account = await addAccount.add({
         name: state.name,
         email: state.email,
         password: state.password,
         passwordConfirmation: state.passwordConfirmation
       })
+      await saveAccessToken.save(account.accessToken)
+      history.replace('/')
     } catch (error) {
       setState({
         ...state,
@@ -105,7 +114,9 @@ const SignUp: React.FC<Props> = ({ validation, addAccount }: Props) => {
           >
             Entrar
           </button>
-          <span className={Styles.link}>Voltar para login</span>
+          <Link to="/login" className={Styles.link}>
+            Voltar para login
+          </Link>
           <FormStatus />
         </form>
       </Context.Provider>
