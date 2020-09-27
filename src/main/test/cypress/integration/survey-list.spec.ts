2c0c5c27c6fab940900337ca0389221db6a4ff1f
Artur Paes <arturopaes@gmail.com>
2020-09-27 14:12:23 -0300

    test: ensure SuverList logout on AccessDeniedError

--- src/main/test/cypress/integration/survey-list.spec.ts
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -11,12 +11,17 @@ describe('SurveyList', () => {
   })
 
   it('Should present error on UnexpectedError', () => {
-    console.log(localStorage.getItem('account'))
     Http.mockUnexpectedError()
     cy.visit('')
     cy.getByTestId('error').should(
       'contain.text',
       'Algo de errado aconteceu. Tente novamente em breve.'
     )
+  })
+
+  it('Should logout on AccessDeniedError', () => {
+    Http.mockAccessDeniedError()
+    cy.visit('')
+    Helper.testUrl('/login')
   })
 })
