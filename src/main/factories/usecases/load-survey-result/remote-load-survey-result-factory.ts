11919fe7bcdf6781157571053a0aedc9a678555f
Artur Paes <arturopaes@gmail.com>
2020-09-29 23:30:14 -0300

    feat: add SurveyResult composition

---
+++ src/main/factories/usecases/load-survey-result/remote-load-survey-result-factory.ts
@@ -0,0 +1,11 @@
+import { LoadSurveyResult } from '@/domain/usecases'
+import { makeApiUrl } from '@/main/factories/http/api-url-factory'
+import { RemoteLoadSurveyResult } from '@/data/usecases'
+import { makeAuthorizeHttpGetClientDecorator } from '@/main/factories/decorators'
+
+export const makeRemoteLoadSurveyResult = (id: string): LoadSurveyResult => {
+  return new RemoteLoadSurveyResult(
+    makeApiUrl(`/surveys/${id}/results`),
+    makeAuthorizeHttpGetClientDecorator()
+  )
+}
