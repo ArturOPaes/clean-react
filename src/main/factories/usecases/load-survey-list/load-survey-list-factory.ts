6fa28f1f9d14f719bef0a0af556fdcc42fb4d8f7
Artur Paes <arturopaes@gmail.com>
2020-10-01 20:12:30 -0300

    refactor: change HttpClient to be more generic

--- src/main/factories/usecases/load-survey-list/load-survey-list-factory.ts
+++ src/main/factories/usecases/load-survey-list/load-survey-list-factory.ts
@@ -1,11 +1,11 @@
 import { LoadSurveyList } from '@/domain/usecases'
 import { makeApiUrl } from '@/main/factories/http/api-url-factory'
 import { RemoteLoadSurveyList } from '@/data/usecases'
-import { makeAuthorizeHttpGetClientDecorator } from '@/main/factories/decorators'
+import { makeAuthorizeHttpClientDecorator } from '@/main/factories/decorators'
 
 export const makeRemoteLoadSurveyList = (): LoadSurveyList => {
   return new RemoteLoadSurveyList(
     makeApiUrl('/surveys'),
-    makeAuthorizeHttpGetClientDecorator()
+    makeAuthorizeHttpClientDecorator()
   )
 }
